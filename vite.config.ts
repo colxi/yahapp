bf4b900513511826731753de06227ff08c77ffa5
Sergi Guzman <sergi.guzman@glovoapp.com>
2026-05-04 01:14:48 +0200

    fix versioning (#5)
    * versioning
    
    * fix pointer

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,16 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
 import { fileURLToPath, URL } from 'node:url';
+import { readFileSync } from 'node:fs';
 
 const base = '/yahapp/';
+const pkg = JSON.parse(readFileSync('./package.json', 'utf-8'));
 
 export default defineConfig({
   base,
+  define: {
+    __APP_VERSION__: JSON.stringify(pkg.version),
+  },
   plugins: [
     react(),
     VitePWA({
